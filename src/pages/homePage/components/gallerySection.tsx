2509b81bf14bc8908fd10c56ca2b5b36d3852f4f
Mats Gundersen <matsgundersen@hotmail.com>
2023-10-09 22:50:40 +0200

    fix of images

--- src/pages/homePage/components/gallerySection.tsx
+++ src/pages/homePage/components/gallerySection.tsx
@@ -1,13 +1,16 @@
 import { Element } from 'react-scroll';
 import PhotoAlbum from "react-photo-album";
-import photo1 from "@/assets/gallery/IMG_4747.jpeg";
-import photo2 from "@/assets/gallery/IMG_4793.jpeg";
-import photo3 from "@/assets/gallery/IMG_8427.jpeg";
-import photo4 from "@/assets/gallery/IMG_8435.jpeg";
-import photo6 from "@/assets/gallery/IMG_8586.jpeg";
-import photo7 from "@/assets/gallery/IMG_8757.jpeg";
-import photo8 from "@/assets/gallery/IMG_8903.jpeg";
-import photo9 from "@/assets/gallery/IMG_8905.jpeg";
+import photo1 from "@/assets/gallery/DJI_0026.webp";
+import photo2 from "@/assets/gallery/DJI_0029.webp";
+import photo3 from "@/assets/gallery/DJI_0036.webp";
+import photo4 from "@/assets/gallery/IMG_4747.webp";
+import photo5 from "@/assets/gallery/IMG_8427.webp";
+import photo6 from "@/assets/gallery/IMG_8435.webp";
+import photo7 from "@/assets/gallery/IMG_8586.webp";
+import photo8 from "@/assets/gallery/IMG_8757.webp";
+import photo9 from "@/assets/gallery/IMG_8903.webp";
+import photo10 from "@/assets/gallery/IMG_8982.webp";
+import photo11 from "@/assets/gallery/IMG_8989.webp";
 import { Fade } from 'react-awesome-reveal';
 
 const photos = [
@@ -32,6 +35,11 @@ const photos = [
       height: 4
     },
     {
+      src: photo5,
+      width: 3,
+      height: 4
+    },
+    {
       src: photo6,
       width: 3,
       height: 4
@@ -43,15 +51,25 @@ const photos = [
     },
     {
       src: photo8,
+      width: 3,
+      height: 4
+    },
+    {
+      src: photo9,
       width: 4,
       height: 3
     },
     {
-      src: photo9,
+      src: photo10,
       width: 3,
       height: 4
     },
-  ];
+    {
+      src: photo11,
+      width: 4,
+      height: 3
+    },
+];
 
 
 const GallerySection : React.FC = () => {
