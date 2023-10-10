cfeda5fbbff0d7a0c8d0e940e74ebbd359bd8834
Mats Gundersen <matsgundersen@hotmail.com>
2023-10-10 18:56:55 +0200

    button changes

--- src/pages/homePage/components/wishListSection.tsx
+++ src/pages/homePage/components/wishListSection.tsx
@@ -19,14 +19,14 @@ const WishListSection : React.FC = () => {
                     duration={3000}
                     triggerOnce={true}>
                     <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-                        <a href="https://onskelister.no/liste?id=9DJpBGcRBSvyvAZ3glRX" className="w-full sm:w-auto bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-md inline-flex items-center justify-center px-4 py-2.5 ">
+                        <a href="https://onskelister.no/liste?id=9DJpBGcRBSvyvAZ3glRX" className="w-full min-w-[200px] sm:w-auto bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-md inline-flex items-center justify-center px-4 py-2.5 ">
                             <GiftIcon className="mr-3 w-7 h-7"/>
                             <div className="text-left">
                                 <div className="mb-1 text-xs">ønskeliste</div>
                                 <div className="-mt-1 font-sans text-sm font-semibold">Ønskelister.no</div>
                             </div>
                         </a>
-                        <a href="https://www.tilbords.no/onskeliste/123312" className="w-full sm:w-auto bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-md inline-flex items-center justify-center px-4 py-2.5">
+                        <a href="https://www.tilbords.no/onskeliste/123312" className="w-full min-w-[200px] sm:w-auto bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-md inline-flex items-center justify-center px-4 py-2.5">
                             <GiftIcon className="mr-3 w-7 h-7"/>
                             <div className="text-left">
                                 <div className="mb-1 text-xs">Ønskeliste</div>
