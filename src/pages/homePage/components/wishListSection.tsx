7a2125d86bc61dd6f4802c4fe3a5fbf3b16c01c6
Mats Gundersen <matsgundersen@hotmail.com>
2023-10-10 18:44:31 +0200

    finnished wishlist

--- src/pages/homePage/components/wishListSection.tsx
+++ src/pages/homePage/components/wishListSection.tsx
@@ -13,17 +13,17 @@ const WishListSection : React.FC = () => {
                     duration={3000}
                     triggerOnce={true}>
                     <h2 className="h2 mb-4 text-primary">Ønskeliste</h2>
-                    <p className="p mb-4 text-gray-500 sm:text-lg ">Vi har laget ønskelister hos Tilbords og Kitchen</p>
+                    <p className="p mb-4 text-gray-500 sm:text-lg ">Vi har laget ønskelister hos Tilbords og Ønskelister.no</p>
                 </Fade>
                 <Fade
                     duration={3000}
                     triggerOnce={true}>
                     <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-                        <a href="#" className="w-full sm:w-auto bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-md inline-flex items-center justify-center px-4 py-2.5 ">
+                        <a href="https://onskelister.no/liste?id=9DJpBGcRBSvyvAZ3glRX" className="w-full sm:w-auto bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-md inline-flex items-center justify-center px-4 py-2.5 ">
                             <GiftIcon className="mr-3 w-7 h-7"/>
                             <div className="text-left">
                                 <div className="mb-1 text-xs">ønskeliste</div>
-                                <div className="-mt-1 font-sans text-sm font-semibold">Kitchen</div>
+                                <div className="-mt-1 font-sans text-sm font-semibold">Ønskelister.no</div>
                             </div>
                         </a>
                         <a href="https://www.tilbords.no/onskeliste/123312" className="w-full sm:w-auto bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-md inline-flex items-center justify-center px-4 py-2.5">
